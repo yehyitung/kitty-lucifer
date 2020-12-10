c3129c6cbfe777e1a9ea0a2d2ccb711ffd3b412f
yehyitung <joe66033166@gmail.com>
2020-12-10 13:17:53 +0800

    add boostrap scss, login api setting

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+import 'bootstrap'
+import App from './App.vue' 
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios);
 
 new Vue({
+  created(){
+    const api = `${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_CUSTOMPATH}/products`;
+    console.log(process.env.VUE_APP_APIPATH, process.env.VUE_APP_CUSTOMPATH);
+    this.$http.get(api).then((response) => {
+      console.log(response.data)
+    })
+  },
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+})
+.$mount('#app')
